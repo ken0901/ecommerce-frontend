b3dc6f2383e21b50478084da628ecdc82e8f3814
ken0901 <ken5260715@gmail.com>
2023-03-07 21:53:54 -0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
