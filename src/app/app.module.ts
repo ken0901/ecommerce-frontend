2bbe6851be4a661bc4fb149a49341fa86892341e
ken0901 <ken5260715@gmail.com>
2023-04-05 20:42:38 -0700

    Security - VIP Member access

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductService } from './services/product.service';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -20,17 +20,27 @@ import { LoginStatusComponent } from './components/login-status/login-status.com
 import{
   OktaAuthModule,
   OktaCallbackComponent,
-  OKTA_CONFIG
+  OKTA_CONFIG,
+  OktaAuthGuard
 } from '@okta/okta-angular';
 
 import { OktaAuth } from '@okta/okta-auth-js';
 import myAppconfig from './config/my-appconfig';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 const oktaConfig = myAppconfig.oidc;
-
 const oktaAuth = new OktaAuth(oktaConfig);
 
+function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector){
+  // Use injector to access any service available within your application
+  const router = injector.get(Router);
+
+  // Redirect the user to your custom login page
+  router.navigate(['/login']);
+}
+
 const routes: Routes = [
+  {path: 'members', component:MembersPageComponent, canActivate:[OktaAuthGuard], data: {onAuthRequired: sendToLoginPage}},
   {path: 'login/callback', component:OktaCallbackComponent},
   {path: 'login', component:LoginComponent},
   {path: 'checkout', component: CheckoutComponent},
@@ -56,6 +66,7 @@ const routes: Routes = [
     CheckoutComponent,
     LoginComponent,
     LoginStatusComponent,
+    MembersPageComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
